9fd2265ea0a739606ca4eb86be0549f13e8d5ceb
Anu Sakpibal <nuhundred@gmail.com>
2018-09-12 15:11:01 +0700

    make it better

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 // Reply with two static messages
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
+const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
+const Router = require('koa-router')
+const router = new Router()
+const app = new Koa()
 const port = process.env.PORT || 4000
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
-app.use(bodyParser.json())
-app.post('/webhook', (req, res) => {
+
+
+router.post('/webhook', (ctx) => {
     console.log("#################################################")
-    console.log(JSON.stringify(req.body))
-    let reply_token = req.body.events[0].replyToken
+    ctx.req.body
     reply(reply_token)
-    res.sendStatus(200)
+    ctx.status(200)
 
 })
-app.listen(port)
+
+
 
 function reply(reply_token) {
     let headers = {
@@ -40,11 +39,22 @@ function reply(reply_token) {
             }
         ]
     })
-    request.post({
+
+    router.get("/ping", ctx => {
+        console.log("object");
+        ctx.body = 'ping'
+    });
+
+    router.post({
         url: 'https://api.line.me/v2/bot/message/reply',
         headers: headers,
         body: body
-    }, (err, res, body) => {
-        console.log('status = ' + res.statusCode);
+    }, (ctx) => {
+        console.log('status = ' + ctx.status);
     });
 }
+
+app.use(router.routes())
+app.listen(port, () => {
+    console.log('server start on port %s', port);
+})
